45c1f85c5d444bf503be4ad672cba9cb0f6611f5
dengbenye <dengbenye@jd.com>
2020-09-14 13:54:53 +0800

    update

--- views/button.js
+++ views/button.js
@@ -1,7 +1,7 @@
 export default (onclick) => {
   const body = document.querySelector('body'),
         button = document.createElement('div')
-  button.style = 'position:fixed;bottom:110px;left:10px;width:56px;height:25px;background:gray;font-size:12px;text-align:center;line-height:23px;'
+  button.style = 'position:fixed;bottom:110px;left:10px;width:56px;height:25px;background:gray;font-size:12px;text-align:center;line-height:23px;z-index:9999;'
   button.appendChild(document.createTextNode('Inspector'))
   button.onclick = onclick
   body && body.appendChild(button)
