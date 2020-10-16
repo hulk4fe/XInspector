c28f29c5a4500adc5e59e7031f1bf97ecf38f16b
dengbenye <dengbenye@jd.com>
2020-10-16 11:34:10 +0800

    新增单例模式支持

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ import { button } from './views'
 
 export default () => {
   try{
-    button(toolkit)
+    if(document.querySelector('.xinspector') === null){
+      button(toolkit)
+    }
   }catch(e){
     console.log('xinspector error --->', e)
   }
